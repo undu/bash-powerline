aa2af96ac35d4b1bf5146efb7d8cc61a6cd0d550
Pau Ruiz i Safont <unduthegun@gmail.com>
2017-02-09 10:29:31 +0000

    maintenance: shellcheck linting

--- powerline.bash
+++ powerline.bash
@@ -51,57 +51,58 @@ __powerline() {
   # Generate terminal colour codes
   # $1 is an int (a colour) and $2 must be 'fg' or 'bg'
   __colour() {
-    case ${2} in
+    case "$2" in
       'fg'*)
-        echo "\[$(tput setaf ${1})\]"
+        echo "\[$(tput setaf "$1")\]"
         ;;
       'bg'*)
-        echo "\[$(tput setab ${1})\]"
+        echo "\[$(tput setab "$1")\]"
         ;;
       *)
-        echo "\[$(tput setab ${1})\]"
+        echo "\[$(tput setab "$1")\]"
         ;;
     esac
   }
 
   # Generate a single-coloured block for the prompt
   __prompt_block() {
-    if [ ! -z ${1+x} ]; then
-      local bg=$1
+    local bg; local fg
+    if [ ! -z "${1+x}" ]; then
+      bg=$1
     else
-      if [ ! -z ${BG} ]; then
-        local bg=$BG
+      if [ ! -z "$BG" ]; then
+        bg=$BG
       else
-        local bg=$DEFAULT_BG
+        bg=$DEFAULT_BG
       fi
     fi
-    if [ ! -z ${2+x} ]; then
-      local fg=$2
+    if [ ! -z "${2+x}" ]; then
+      fg=$2
     else
-      local fg=$DEFAULT_FG
+      fg=$DEFAULT_FG
     fi
 
     local block
 
     # Need to generate a separator if the background changes
-    if [[ ! -z $BG && $bg != $BG && ! -z ${POWERLINE_FONT+x} ]]; then
-      block+="$(__colour $bg 'bg')"
-      block+="$(__colour $BG 'fg')"
+    if [[ ! -z "$BG" && "$bg" != "$BG" && ! -z "${POWERLINE_FONT+x}" ]]; then
+      block+="$(__colour "$bg" 'bg')"
+      block+="$(__colour "$BG" 'fg')"
       block+="$BLOCK_START $RESET"
-      block+="$(__colour $bg 'bg')"
-      block+="$(__colour $fg 'fg')"
+      block+="$(__colour "$bg" 'bg')"
+      block+="$(__colour "$fg" 'fg')"
     else
-      block+="$(__colour $bg 'bg')"
-      block+="$(__colour $fg 'fg')"
+      block+="$(__colour "$bg" 'bg')"
+      block+="$(__colour "$fg" 'fg')"
       block+=" "
     fi
 
-    if [ ! -z ${3+x} ]; then
-      block+="${3} $RESET"
+    if [ ! -z "${3+x}" ]; then
+      block+="$3 $RESET"
     fi
 
     BG=$bg
-    echo $block
+    echo "$block"
   }
 
   ### Prompt components
@@ -114,7 +115,7 @@ __powerline() {
     # force git output in English to make our work easier
     local git_eng="env LANG=C git"
     # get current branch name or short SHA1 hash for detached head
-    local branch="$($git_eng symbolic-ref --short HEAD 2>/dev/null || $git_eng describe --tags --always 2>/dev/null)"
+    local branch; branch="$($git_eng symbolic-ref --short HEAD 2>/dev/null || $git_eng describe --tags --always 2>/dev/null)"
 
     if [ "x$branch" == "x" ]; then
       # git branch not found
@@ -127,22 +128,23 @@ __powerline() {
     [ -n "$($git_eng status --porcelain 2>/dev/null)" ] && marks+=" $GIT_BRANCH_CHANGED_SYMBOL"
 
     # how many commits local branch is ahead/behind of remote?
-    local stat="$($git_eng status --porcelain --branch 2>/dev/null | grep '^##' | grep -o '\[.\+\]$')"
-    local aheadN="$(echo $stat | grep -o 'ahead [[:digit:]]\+' | grep -o '[[:digit:]]\+')"
-    local behindN="$(echo $stat | grep -o 'behind [[:digit:]]\+' | grep -o '[[:digit:]]\+')"
+    local stat; local aheadN; local behindN
+    stat="$($git_eng status --porcelain --branch 2>/dev/null | grep '^##' | grep -o '\[.\+\]$')"
+    aheadN="$(echo "$stat" | grep -o 'ahead [[:digit:]]\+' | grep -o '[[:digit:]]\+')"
+    behindN="$(echo "$stat" | grep -o 'behind [[:digit:]]\+' | grep -o '[[:digit:]]\+')"
     [ -n "$aheadN" ] && marks+=" $GIT_NEED_PUSH_SYMBOL$aheadN"
     [ -n "$behindN" ] && marks+=" $GIT_NEED_PULL_SYMBOL$behindN"
 
+    local bg; local fg
+    fg=$(__colour $BLACK 'fg')
     if [ -z "$marks" ]; then
-      local bg=$(__colour $GREEN 'bg')
-      local fg=$(__colour $BLACK 'fg')
+      bg=$(__colour $GREEN 'bg')
     else
-      local bg=$(__colour $YELLOW 'bg')
-      local fg=$(__colour $BLACK 'fg')
+      bg=$(__colour $YELLOW 'bg')
     fi
 
     # print the git branch segment without a trailing newline
-    printf "$bg$fg $GIT_BRANCH_SYMBOL $branch$marks "
+    echo "$bg$fg $GIT_BRANCH_SYMBOL $branch$marks "
   }
 
   __virtualenv_block() {
@@ -150,27 +152,28 @@ __powerline() {
     # https://github.com/pypa/virtualenv/blob/a9b4e673559a5beb24bac1a8fb81446dd84ec6ed/virtualenv_embedded/activate.sh#L62
     # License: MIT
     if [ -n "$VIRTUAL_ENV" ]; then
-      if [ "`basename \"$VIRTUAL_ENV\"`" == "__" ]; then
+      local text
+      if [ "$(basename \""$VIRTUAL_ENV"\")" == "__" ]; then
         # special case for Aspen magic directories
         # see http://www.zetadev.com/software/aspen/
-        local text="[`basename \`dirname \"$VIRTUAL_ENV\"\``]"
+        text="[$(basename \$\(dirname \""$VIRTUAL_ENV"\"\))]"
       else
-        local text="(`basename \"$VIRTUAL_ENV\"`)"
+        text="($(basename \""$VIRTUAL_ENV"\"))"
       fi
-      echo $(__prompt_block $WHITE $BLACK $text)
+      __prompt_block $WHITE $BLACK "$text"
     fi
   }
 
   __pwd() {
     # Use ~ to represent $HOME prefix
-    local pwd=$(pwd | sed -e "s|^$HOME|~|")
+    local pwd; pwd=$(pwd | sed -e "s|^$HOME|~|")
     if [[ ( $pwd = ~\/*\/* || $pwd = \/*\/*/* ) && ${#pwd} -gt $MAX_PATH_LENGTH ]]; then
       local IFS='/'
       read -ra split <<< "$pwd"
       if [[ $pwd = ~* ]]; then
-        pwd="~/${split[1]}/.../${split[@]:(-2):1}/${split[@]:(-1)}"
+        pwd="~/${split[1]}/.../${split[*]:(-2):1}/${split[*]:(-1)}"
       else
-        pwd="/${split[1]}/.../${split[@]:(-2):1}/${split[@]:(-1)}"
+        pwd="/${split[1]}/.../${split[*]:(-2):1}/${split[*]:(-1)}"
       fi
     fi
     echo "$pwd"
@@ -183,11 +186,11 @@ __powerline() {
     # Decide colours to use
     local fg=$WHITE_BRIGHT
     local bg=$BLUE
-    if [ $(whoami) == "root" ]; then
+    if [ "$(whoami)" == "root" ]; then
       local bg=$RED
     fi
 
-    if [ $(whoami) == "root" ]; then
+    if [ "$(whoami)" == "root" ]; then
       local show_user="y"
       local show_host="y"
     fi
@@ -206,7 +209,7 @@ __powerline() {
       text+="@\h"
     fi
 
-    echo $(__prompt_block $bg $fg $text)
+    __prompt_block $bg $fg $text
   }
 
   __status_block() {
@@ -214,24 +217,24 @@ __powerline() {
     $(history -a ; history -n)
 
     if [ $exit_code -ne 0 ]; then
-      echo $(__prompt_block $BLACK $RED '✘')
+      __prompt_block $BLACK $RED '✘'
     fi
 
-    local uid=$(id -u $USER)
-    if [ $uid -eq 0 ]; then
-      echo $(__prompt_block $BLACK $YELLOW '⚡')
+    local uid; uid=$(id -u "$USER")
+    if [ "$uid" -eq 0 ]; then
+      __prompt_block $BLACK $YELLOW '⚡'
     fi
 
-    local jobs=$(jobs -l | wc -l)
-    if [ $jobs -gt 0 ]; then
-      echo $(__prompt_block $BLACK $CYAN '⚙')
+    local jobs; jobs=$(jobs -l | wc -l)
+    if [ "$jobs" -gt 0 ]; then
+      __prompt_block $BLACK $CYAN '⚙'
     fi
   }
 
   # Build the prompt
   prompt() {
     # I don't like bash; execute first to capture correct status code
-    local status_block="$(__status_block)"
+    local status_block; status_block="$(__status_block)"
 
     PS1="\n"
     PS1+=$status_block
